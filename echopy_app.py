f12f26ef82b588506ef5d29b4dd4bde13f31b2b1
Zachary Priddy <me@zpriddy.com>
2015-07-10 00:58:22 -0700

    Start Setup for SSL on Flask

--- echopy_app.py
+++ echopy_app.py
@@ -1,7 +1,22 @@
-def run(app):
+import ssl 
 
+ssl_enable = False
+
+def run(app):
     try:
-        app.run(debug=True,
+        if ssl_enable:
+            context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+            context.load_cert_chain('yourserver.crt', 'yourserver.key')
+            app.run(debug=True,
+                    port=5000,
+                    threaded=True,
+                    use_reloader=False,
+                    use_debugger=True,
+                    ssl_context=context,
+                    host='0.0.0.0'
+                    )
+        else:
+            app.run(debug=True,
                 port=5000,
                 threaded=True,
                 use_reloader=False,
